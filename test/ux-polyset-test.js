3c4a24b2a620860c2edd55105b9fb0b2dd0cf75e
Michael Siebert <siebertm85@googlemail.com>
2010-04-03 17:36:58 +0200

    add extjs 3.2.0 compatibility

--- test/ux-polyset-test.js
+++ test/ux-polyset-test.js
@@ -13,9 +13,13 @@ Ext.onReady(function() {
       xtype: "form",
       items: [{
         xtype: "ux-polyset",
-        addButtonConfig: {
-          text: "new"
-        },
+        addButtons: [{
+          text: "new1",
+          addValue: 42
+        }, {
+          text: 'new2',
+          addValue: 'bar'
+        }],
         removeButtonConfig: {
           text: "go away"
         },
@@ -41,7 +45,32 @@ Ext.onReady(function() {
           };
         },
         value: [1, 2, 3, 4]
-      }],
+      }, {
+           xtype: 'fieldset',
+            title: 'Individual Checkboxes',
+            autoHeight: true,
+            defaultType: 'checkbox', // each item will be a checkbox
+            items: [{
+                xtype: 'textfield',
+                name: 'txt-test1',
+                fieldLabel: 'Alignment Test'
+            }, {
+                fieldLabel: 'Favorite Animals',
+                // boxLabel: 'Dog',
+                name: 'fav-animal-dog'
+            }, {
+                fieldLabel: '',
+                labelSeparator: '',
+                boxLabel: 'Cat',
+                name: 'fav-animal-cat'
+            }, {
+                checked: true,
+                fieldLabel: '',
+                labelSeparator: '',
+                boxLabel: 'Monkey',
+                name: 'fav-animal-monkey'
+            }]
+             }],
       buttons: [{
         text: "get value",
         handler: function() {
